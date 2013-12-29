acdb438120e60c3f216b5e016a612665e49bb0c4
Rohit Bhore <rohit.bhore@icicletech.com>
2013-12-30 00:30:12 +0530

    [controller spec] changes to the authontication callbacks

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,29 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-DEFAULT_PASSWORD = 'password'
+# # Read about factories at https://github.com/thoughtbot/factory_girl
+# DEFAULT_PASSWORD = 'password'
+# FactoryGirl.define do
+#   factory :user do
+#   	sequence(:email){ |n| "person_#{n}@example.com"}
+#     password              DEFAULT_PASSWORD
+#     password_confirmation DEFAULT_PASSWORD
+
+#     factory :admin do
+#       admin true
+#     end
+
+#   end
+# end
+
+require 'faker'
+
 FactoryGirl.define do
   factory :user do
-  	sequence(:email){ |n| "person_#{n}@example.com"}
-    password              DEFAULT_PASSWORD
-    password_confirmation DEFAULT_PASSWORD
+    email { Faker::Internet.email }
+    password 'password'
+    password_confirmation 'password'
+
 
     factory :admin do
       admin true
     end
-
   end
 end
