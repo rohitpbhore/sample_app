b7eaaa19eea5d2e841781d603def262d12bd1e41
Rohit Bhore <rohit.bhore@icicletech.com>
2013-12-31 03:16:10 +0530

    [controller spec] settings for Devise

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,10 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  RSpec.configure do |config|
+    config.include Devise::TestHelpers, :type => :controller
+  end
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
