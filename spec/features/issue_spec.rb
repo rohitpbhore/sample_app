97c30f23cdf51b51bfca03bd23d11544f7fe8180
Rohit Bhore <rohit.bhore@icicletech.com>
2013-12-31 12:51:58 +0530

    [Contoller spec] Add macros, and matchers

--- spec/features/issue_spec.rb
+++ spec/features/issue_spec.rb
@@ -2,14 +2,7 @@ require 'spec_helper'
 
 describe "issues" do
 	before do
-    visit issues_path
-    click_link 'Sign up'
-    fill_in 'user[email]', :with => 'example@gg.com'
-    fill_in 'user[password]', :with => '12345678'
-    fill_in 'user[password_confirmation]', :with => '12345678'
-    click_button 'Sign up'
-    page.should have_content 'Welcome! You have signed up successfully.'
-
+    sign_in
     @issue = FactoryGirl.create(:issue)
 	end
 
