2ba47f684f15875dba0fba43eb7e59bf3c63b312
Rohit Bhore <rohit.bhore@icicletech.com>
2014-01-08 00:49:47 +0530

    [model spec] Reset password model spec with devise

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -9,7 +9,7 @@ describe UserMailer do
       mail.subject.should eq("Password Reset")
       mail.to.should eq([user.email])
       mail.from.should eq(["from@example.com"])
-      mail.body.encoded.should match(edit_password_reset_path(user.reset_password_token))
+      mail.body.encoded.should match(edit_user_password_path(user.reset_password_token))
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,19 +5,19 @@ describe User do
     let(:user) { FactoryGirl.create(:user) }
 
     it "generates a unique password_reset_token each time" do
-      user.send_password_reset
-      last_token = user.password_reset_token
-      user.send_password_reset
-      user.password_reset_token.should_not eq(last_token)
+      user.send_reset_password_instructions
+      last_token = user.reset_password_token
+      user.send_reset_password_instructions
+      user.reset_password_token.should_not eq(last_token)
     end
 
     it "saves the time the password reset was sent" do
-      user.send_password_reset
-      user.reload.password_reset_sent_at.should be_present
+      user.send_reset_password_instructions
+      user.reload.reset_password_sent_at.should be_present
     end
 
     it "delivers email to user" do
-      user.send_password_reset
+      user.send_reset_password_instructions
       last_email.to.should include(user.email)
     end
   end
