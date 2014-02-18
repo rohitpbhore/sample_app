d712d8ca0c2edbea76dc1367a93684c197b13eb3
Rohit Bhore <rohit.bhore@icicletech.com>
2014-02-18 22:11:12 +0530

    start with like/unlike functionality.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140201063747) do
+ActiveRecord::Schema.define(:version => 20140218122226) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(:version => 20140201063747) do
     t.datetime "updated_at",                  :null => false
     t.integer  "project_id",   :default => 1
     t.string   "tags"
+  end
+
+  create_table "likes", :force => true do |t|
+    t.integer  "issue_id"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "timelines", :force => true do |t|
@@ -56,6 +63,10 @@ ActiveRecord::Schema.define(:version => 20140201063747) do
     t.datetime "updated_at",                             :null => false
     t.string   "authentication_token"
     t.string   "auto_token"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
