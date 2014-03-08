00922efd37da979d3efab23bdfee64d719dec7d0
Rohit Bhore <rohit.bhore@icicletech.com>
2014-03-09 00:16:01 +0530

    comment model spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without body" do
+    comment = FactoryGirl.build(:comment, body: nil)
+    expect(comment).to have(1).errors_on(:body)
+  end
+
+  it "is valid with body" do
+    comment = FactoryGirl.build(:comment)
+    expect(FactoryGirl.build(:comment)).to be_valid
+  end
 end
