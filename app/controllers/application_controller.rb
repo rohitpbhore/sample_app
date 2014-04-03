faffa8bc002fcc547b0cdfdd0d18293bb0521a52
Rohit Bhore <rohit.bhore@icicletech.com>
2014-04-03 14:53:00 +0530

    make up to the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationControll < ActionController::Base
+class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
